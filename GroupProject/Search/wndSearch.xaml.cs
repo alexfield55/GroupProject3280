ec164f42db3e7e0c903c8790b19020f6688c0e42
Alex Field <38266956+alexfield55@users.noreply.github.com>
2021-07-25 12:47:32 -0600

    Initial commit

---
+++ GroupProject/Search/wndSearch.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GroupProject.Search
+{
+    /// <summary>
+    /// Interaction logic for wndSearch.xaml
+    /// </summary>
+    public partial class wndSearch : Window
+    {
+        public wndSearch()
+        {
+            InitializeComponent();
+        }
+    }
+}
